52c98bbb40b4b6f90eb99d55b0cc1493aba1e1b7
Thiago Lima <thiagotigaz@gmail.com>
2019-09-22 18:17:01 -0700

    Add more tests

--- src/test/java/com/wpspublish/customer/CustomerControllerTest.java
+++ src/test/java/com/wpspublish/customer/CustomerControllerTest.java
@@ -1,12 +1,26 @@
 package com.wpspublish.customer;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -18,6 +32,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -29,13 +44,19 @@ public class CustomerControllerTest {
     ArgumentCaptor<Pageable> pageableCaptor;
 
     @MockBean
-    private CustomerRepository customerRepository;
-
-    @MockBean
-    private CustomerService customerService;
+    private CustomerServiceImpl customerService;
 
     @Autowired
     private MockMvc mockMvc;
+
+    private static final Long CUSTOMER_ID = 100L;
+    private static final String FIRST_NAME = "Talita";
+    private static final String LAST_NAME = "Lima";
+    private static final String PROFESSION = "Business Consultant";
+    private static final int AGE = 35;
+    private static final Customer CUSTOMER = new Customer(CUSTOMER_ID, FIRST_NAME, LAST_NAME, PROFESSION, AGE);
+    private static final CustomerDto CUSTOMER_DTO =
+            new CustomerDto(FIRST_NAME, LAST_NAME, PROFESSION, AGE, CUSTOMER.getDateCreated());
 
     @Test
     public void evaluatesPageableParameter() throws Exception {
@@ -58,13 +79,90 @@ public class CustomerControllerTest {
     }
 
     @Test
+    public void findByIdShouldReturnOk() throws Exception {
+        when(customerService.find(CUSTOMER_ID)).thenReturn(Optional.of(CUSTOMER_DTO));
+        // Act and assert
+        mockMvc.perform(get(String.format("/customer/%s", CUSTOMER_ID)))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString(FIRST_NAME)))
+                .andExpect(content().string(containsString(LAST_NAME)))
+                .andExpect(content().string(containsString(PROFESSION)))
+                .andExpect(content().string(containsString(String.valueOf(AGE))));
+        verify(customerService).find(CUSTOMER_ID);
+    }
+
+    @Test
+    public void findByIdShouldReturnNotFoundWhenCustomerDoestExist() throws Exception {
+        when(customerService.find(CUSTOMER_ID)).thenReturn(Optional.empty());
+        // Act and assert
+        mockMvc.perform(get(String.format("/customer/%s", CUSTOMER_ID)))
+                .andExpect(status().isNotFound());
+        verify(customerService).find(CUSTOMER_ID);
+    }
+
+    @Test
     public void createCustomerShouldReturnLocationHeader() throws Exception {
-        // Arrange and Act
+        // Arrange
+        when(customerService.createCustomer(any())).thenReturn(CUSTOMER);
+        String payload = loadJson("customer-payload.json");
+        // Act and assert
         mockMvc.perform(post("/customer")
-                .param("page", "5")
-                .param("size", "10")
-                .param("sort", "id,desc")   // <-- no space after comma!
-                .param("sort", "first_name,asc")) // <-- no space after comma!
-                .andExpect(status().isCreated());
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", containsString("http://localhost/customer/100")));
+        verify(customerService).createCustomer(any(CustomerDto.class));
+    }
+
+    @Test
+    public void updateCustomerShouldReturnOk() throws Exception {
+        // Arrange
+        String payload = loadJson("customer-payload.json");
+        // Act
+        mockMvc.perform(put(String.format("/customer/%s", CUSTOMER_ID))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload))
+                .andExpect(status().isOk());
+        verify(customerService).updateCustomer(eq(CUSTOMER_ID), any(CustomerDto.class));
+    }
+
+    @Test
+    public void updateCustomerShouldReturnBadRequestWhenCustomerDoesntExist() throws Exception {
+        // Arrange
+        doThrow(CustomerBadRequestException.class).when(customerService)
+                .updateCustomer(eq(CUSTOMER_ID), any(CustomerDto.class));
+        String payload = loadJson("customer-payload.json");
+        // Act and assert
+        mockMvc.perform(put(String.format("/customer/%s", CUSTOMER_ID))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload))
+                .andExpect(status().isBadRequest());
+        verify(customerService).updateCustomer(eq(CUSTOMER_ID), any(CustomerDto.class));
+    }
+
+    @Test
+    public void deleteCustomerShouldReturnNoContent() throws Exception {
+        // Arrange
+        doThrow(CustomerBadRequestException.class).when(customerService).deleteCustomer(CUSTOMER_ID);
+        // Act and assert
+        mockMvc.perform(delete(String.format("/customer/%s", CUSTOMER_ID)))
+                .andExpect(status().isBadRequest());
+        verify(customerService).deleteCustomer(CUSTOMER_ID);
+    }
+
+    @Test
+    public void deleteCustomerShouldReturnBadRequestWhenCustomerDoesntExist() throws Exception {
+        // Arrange
+        doThrow(CustomerBadRequestException.class).when(customerService)
+                .deleteCustomer(CUSTOMER_ID);
+        // Act and assert
+        mockMvc.perform(delete(String.format("/customer/%s", CUSTOMER_ID)))
+                .andExpect(status().isBadRequest());
+        verify(customerService).deleteCustomer(CUSTOMER_ID);
+    }
+
+    private String loadJson(String file) throws Exception {
+        Path path = Paths.get(getClass().getClassLoader().getResource(file).toURI());
+        return Files.lines(path).collect(Collectors.joining("\n"));
     }
 }
